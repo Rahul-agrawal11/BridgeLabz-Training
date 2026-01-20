159417dbb5ef7cca4c77632551da8fce4ca5e3ce
Rahul Agrawal <agrawalrahul11112004@gmail.com>
2026-01-20 22:31:58 +0530

    [Rahul Agrawal] Add. leetcode problems solved.

---
+++ dsa-practice/leetcode/BinarySearch.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+//Binary Search (LeetCode #704)
+
+public class BinarySearch {
+
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        System.out.println(search(nums, 9));
+    }
+}
+
